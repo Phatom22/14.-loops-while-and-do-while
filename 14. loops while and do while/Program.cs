d3be07ed858811aee052a0aeb996048d41b6f802
nomvula <nzshabangu95@gmail.com>
2023-09-13 10:45:27 +0200

    Add project files.

---
+++ 14. loops while and do while/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.loops_while_and_do_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 0;
+            while( index <= 500 )
+            {
+                
+                Console.WriteLine(index);
+                index++;
+            }
+
+            Console.WriteLine("this is now do while loop");
+            int index2 = 0;
+                do
+            {
+                Console.WriteLine(index2);
+                index2++;
+            }
+            while (index2 <= 5);
+
+            Console.ReadLine();
+        }
+    }
+}
